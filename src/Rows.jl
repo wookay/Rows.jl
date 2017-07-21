9fd0c04e7b9195a7264918a8a389cb68892e14b3
WooKyoung Noh <wookay.noh@gmail.com>
2017-07-22 06:45:00 +0900

    reshape rows

--- src/Rows.jl
+++ src/Rows.jl
@@ -4,7 +4,7 @@ module Rows
 
 export rows, columns
 
-rows(M::Matrix) = map(x->getindex(M, x, :)', 1:size(M)[1])
+rows(M::Matrix) = map(x->reshape(getindex(M, x, :), :, size(M)[2]), 1:size(M)[1])
 columns(M::Matrix) = map(x->getindex(M, :, x), 1:size(M)[2])
 
 end # module
